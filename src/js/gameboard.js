9675c25ed6fea99f620943f294da578698b68e17
gabs <gabs712.g@gmail.com>
2024-12-09 16:21:27 -0300

    Add gameboard factory

---
+++ src/js/gameboard.js
@@ -0,0 +1,23 @@
+const Gameboard = () => {
+  const slots = createSlots()
+
+  const createBoard = () => {
+    return
+  }
+
+  const placeShipAt = (coordinate) => {
+    return
+  }
+
+  const receiveAttack = (coordinate) => {
+    return
+  }
+
+  const isAllSunk = () => {
+    return
+  }
+
+  return { placeShipAt, receiveAttack, isAllSunk, slots }
+}
+
+export default Gameboard
