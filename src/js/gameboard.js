6717f3393e239e632c1525076d50e92bacf7a105
gabs <gabs712.g@gmail.com>
2024-12-09 16:22:34 -0300

    Add create slots function

--- src/js/gameboard.js
+++ src/js/gameboard.js
@@ -1,3 +1,15 @@
+const createSlots = () => {
+  const slots = []
+
+  for (let i = 0; i < 10; i++) {
+    slots[i] = []
+    for (let j = 0; j < 10; j++) {
+      slots[i][j] = null
+    }
+  }
+  return slots
+}
+
 const Gameboard = () => {
   const slots = createSlots()
 
