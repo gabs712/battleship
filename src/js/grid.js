be2ebb00040d6776e0c68bbc5cefd670b011bdeb
gabs <gabs712.g@gmail.com>
2024-12-12 18:42:06 -0300

    Set row and column atributes to grid elements

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,43 +1,52 @@
 // Ships of size: 5, 4, 3, 3, 2
 
 const Grid = (gridElement, player) => {
-  const handleShip = (el) => {
-    return
-  }
-
-  const handleEmpty = (el) => {
-    return
-  }
-
-  const handleMissed = (el) => {
-    return
-  }
-
-  const getItemElement = (item) => {
-    const el = document.createElement('div')
+  const handleDefaultCel = (el, row, column) => {
     el.className = `
       cursor-pointer hover:bg-gray-200 outline-slate-500 outline-2 outline bg-gray-100
       active:bg-gray-300 
     `
+    el.setAttribute('row', row)
+    el.setAttribute('column', column)
+  }
+
+  const handleShipCel = (el) => {
+    return
+  }
+
+  const handleEmptyCel = (el) => {
+    return
+  }
+
+  const handleMissedCel = (el) => {
+    return
+  }
+
+  const getItemElement = (item, row, column) => {
+    const el = document.createElement('div')
+
+    handleDefaultCel(el, row, column)
+
     if (item.type === 'ship') {
-      handleShip(el)
+      handleShipCel(el)
     }
 
     if (item.type === 'empty') {
-      handleEmpty(el)
+      handleEmptyCel(el)
     }
 
     if (item.type === 'missed') {
-      handleMissed(el)
+      handleMissedCel(el)
     }
 
     return el
   }
 
   const renderSlot = () => {
-    for (const row of player.gameboard.slots) {
-      for (const item of row) {
-        gridElement.append(getItemElement(item))
+    for (const [i, row] of player.gameboard.slots.entries()) {
+      for (const [j, item] of row.entries()) {
+        const element = getItemElement(item, i, j)
+        gridElement.append(element)
       }
     }
   }
