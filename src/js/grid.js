307459c9a8314fa56f780ffb54227f9906087da4
gabs <gabs712.g@gmail.com>
2024-12-12 17:49:07 -0300

    Update grid factory structure

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,29 +1,49 @@
 // Ships of size: 5, 4, 3, 3, 2
 
-const Grid = () => {
-  const grid1 = document.querySelector('[data-grid1]')
-  const grid2 = document.querySelector('[data-grid2]')
+const Grid = (gridElement, player) => {
+  const handleShip = (el) => {
+    return
+  }
+
+  const handleEmpty = (el) => {
+    return
+  }
+
+  const handleMissed = (el) => {
+    return
+  }
 
   const getItemElement = (item) => {
     const el = document.createElement('div')
     el.className = `
-cursor-pointer hover:bg-gray-200 outline-slate-500 outline-2 outline bg-gray-100
-active:bg-gray-300 
-`
+      cursor-pointer hover:bg-gray-200 outline-slate-500 outline-2 outline bg-gray-100
+      active:bg-gray-300 
+    `
+    if (item.type === 'ship') {
+      handleShip(el)
+    }
+
+    if (item.type === 'empty') {
+      handleEmpty(el)
+    }
+
+    if (item.type === 'missed') {
+      handleMissed(el)
+    }
+
     return el
   }
 
-  const renderSlot = (grid, slot) => {
-    for (const row of slot) {
+  const renderSlot = () => {
+    for (const row of player.gameboard.slots) {
       for (const item of row) {
-        grid.append(getItemElement(item))
+        gridElement.append(getItemElement(item))
       }
     }
   }
 
-  const setup = (slot1, slot2) => {
-    renderSlot(grid1, slot1)
-    renderSlot(grid2, slot2)
+  const setup = () => {
+    renderSlot()
   }
 
   return { setup }
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
 import Grid from './grid'
 import Player from './player'
 
-const grid = Grid()
 const player1 = Player(false)
 const player2 = Player(true)
+const gridElement1 = document.querySelector('[data-grid1]')
+const gridElement2 = document.querySelector('[data-grid2]')
 
-grid.setup(player1.gameboard.slots, player2.gameboard.slots)
+const grid1 = Grid(gridElement1, player1)
+const grid2 = Grid(gridElement2, player2)
+
+grid1.setup()
+grid2.setup()
