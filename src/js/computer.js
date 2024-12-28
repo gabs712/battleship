47274323f8317d9325d1c218be62ea74f21be267
gabs <gabs712.g@gmail.com>
2024-12-28 12:10:03 -0300

    Add computer attack logic

--- src/js/computer.js
+++ src/js/computer.js
@@ -1,8 +1,32 @@
-const Computer = () => {
+const attacked = []
+
+const Computer = (player) => {
   const grid = document.querySelector(`[data-grid2]`)
 
+  const isValidCombination = (row, column) => {
+    const slot = player.gameboard.slots[row][column]
+    console.log(slot)
+
+    if (slot.type === 'missed') {
+      console.log(1)
+      return false
+    }
+
+    return true
+  }
+
   const attack = () => {
-    const el = grid.querySelector(`[row="0"][column="9"]`)
+    let row
+    let column
+
+    do {
+      row = Math.floor(Math.random() * 10)
+      column = Math.floor(Math.random() * 10)
+    } while (!isValidCombination(row, column))
+
+    attacked.push([row, column])
+
+    const el = grid.querySelector(`[row="${row}"][column="${column}"]`)
     el.obj.receiveAttack()
   }
   return { attack }
