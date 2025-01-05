aa41723c5d8f0fd87305574b20605bcce19983c8
gabs <gabs712.g@gmail.com>
2025-01-05 18:32:45 -0300

    onst value of arr) {

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,6 @@
-// Ships of size: 5, 4, 3, 3, 2
-
 import Grid from './grid'
 import Player from './player'
 import { RandomizeShips } from './randomize'
-import Ship from './ship'
 
 const gridElement1 = document.querySelector('[data-grid1]')
 const gridElement2 = document.querySelector('[data-grid2]')
@@ -11,8 +8,6 @@ const gridElement2 = document.querySelector('[data-grid2]')
 const player1 = Player(true)
 const player2 = Player(false)
 
-const grid1 = Grid(gridElement1, player1)
-const grid2 = Grid(gridElement2, player2)
 
 player1.enemy = player2
 player2.enemy = player1
